061e6ed8b87d783f1cbb0cf25f21a8c43d80af03
cpham44 <92818518+cpham44@users.noreply.github.com>
2022-04-18 15:41:49 -0700

    camera functionality implemented

---
+++ Naturely/Controllers/PostCell.swift
@@ -0,0 +1,28 @@
+//
+//  PostCell.swift
+//  Naturely
+//
+//  Created by Christine  on 4/18/22.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+
+    @IBOutlet weak var photoView: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
