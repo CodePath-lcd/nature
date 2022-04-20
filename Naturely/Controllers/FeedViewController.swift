73e8e1ccb7412a3820d897fbe51f974e24b47475
cpham44 <92818518+cpham44@users.noreply.github.com>
2022-04-19 18:15:40 -0700

    user can logout

--- Naturely/Controllers/FeedViewController.swift
+++ Naturely/Controllers/FeedViewController.swift
@@ -119,7 +119,7 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         
         cell.usernameLabel.text = user.username
         
-        cell.captionLabel.text = post["caption"] as! String
+            cell.captionLabel.text = post["caption"] as? String
         
         let imageFile = post["image"] as! PFFileObject
         let urlString = imageFile.url!
@@ -163,8 +163,8 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     @IBAction func onLogoutButton(_ sender: Any) {
         PFUser.logOut()
         
-        let main = UIStoryboard(name: "FeedScreen", bundle: nil)
-        let loginViewController = main.instantiateViewController(identifier: "Main")
+        let main = UIStoryboard(name: "Main", bundle: nil)
+        let loginViewController = main.instantiateViewController(identifier: "LoginViewController")
         guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene, let
                 delegate = windowScene.delegate as? SceneDelegate else {return}
         delegate.window?.rootViewController = loginViewController
