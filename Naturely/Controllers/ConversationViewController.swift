ba5ac426c274abe34748388d9b11531aa9042e5e
LuisMora8 <moraluis1325@gmail.com>
2022-04-27 19:56:40 -0700

    Messages and Sender struct files

--- Naturely/Controllers/ConversationViewController.swift
+++ Naturely/Controllers/ConversationViewController.swift
@@ -1,60 +1,43 @@
-////
-////  ConversationViewController.swift
-////  Naturely
-////
-////  Created by Ditry D'ateain on 4/24/22.
-////
 //
-//import UIKit
-//import MessageKit
+//  ConversationViewController.swift
+//  Naturely
 //
-//struct Message: MessageType {
+//  Created by Ditry D'ateain on 4/24/22.
 //
-//    var sender: SenderType
-//    var messageId: String
-//    var sentDate: Date
-//    var kind: MessageKind
-//
-//}
-//
-//struct Sender: SenderType {
-//
-//    var photoURL: String
-//    var senderId: String
-//    var displayName: String
-//
-//}
-//
-//class ConversationViewController: MessagesViewController {
-//
-//    private var messages = [Message]()
-//
-//    private let selfSender = Sender(photoURL: "", senderId: "1", displayName: "John")
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//
-//        messages.append(Message(sender: selfSender, messageId: "1", sentDate: Date(), kind: .text("placeholder")))
-//
-//        messagesCollectionView.messagesDataSource = self
-//        messagesCollectionView.messagesLayoutDelegate = self
-//        messagesCollectionView.messagesDisplayDelegate = self
-//
-//    }
-//
-//}
-//
-//extension ConversationViewController: MessagesDataSource, MessagesLayoutDelegate, MessagesDisplayDelegate {
-//    func currentSender() -> SenderType {
-//        return selfSender
-//    }
-//
-//    func messageForItem(at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> MessageType {
-//        return messages[indexPath.section]
-//    }
-//
-//    func numberOfSections(in messagesCollectionView: MessagesCollectionView) -> Int {
-//        return messages.count
-//    }
-//
-//}
+
+import UIKit
+import MessageKit
+
+class ConversationViewController: MessagesViewController {
+
+    private var messages = [Message]()
+
+    private let selfSender = Sender(photoURL: "", senderId: "1", displayName: "John")
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        messages.append(Message(sender: selfSender, messageId: "1", sentDate: Date(), kind: .text("placeholder")))
+
+        messagesCollectionView.messagesDataSource = self
+        messagesCollectionView.messagesLayoutDelegate = self
+        messagesCollectionView.messagesDisplayDelegate = self
+
+    }
+
+}
+
+extension ConversationViewController: MessagesDataSource, MessagesLayoutDelegate, MessagesDisplayDelegate {
+    func currentSender() -> SenderType {
+        return selfSender
+    }
+
+    func messageForItem(at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> MessageType {
+        return messages[indexPath.section]
+    }
+
+    func numberOfSections(in messagesCollectionView: MessagesCollectionView) -> Int {
+        return messages.count
+    }
+
+}
---
+++ Naturely/Model/Sender.swift
@@ -0,0 +1,17 @@
+//
+//  Sender.swift
+//  Naturely
+//
+//  Created by Luis Mora on 4/27/22.
+//
+
+import Foundation
+import MessageKit
+
+struct Sender: SenderType {
+
+    var photoURL: String
+    var senderId: String
+    var displayName: String
+
+}
